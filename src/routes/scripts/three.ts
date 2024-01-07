c3439c6a48fd4abbe270fc968b667b4a871b97f2
defrost <118262248+defrxst@users.noreply.github.com>
2024-01-07 14:44:20 +0000

    added grid

--- src/routes/scripts/three.ts
+++ src/routes/scripts/three.ts
@@ -1,4 +1,5 @@
 import { browser } from '$app/environment'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 import * as THREE from 'three'
 
 if(browser) {
@@ -7,13 +8,29 @@ if(browser) {
     const scene = new THREE.Scene()
     const renderer = new THREE.WebGLRenderer()
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
+    
+    camera.position.z = 20
+    camera.position.y = 5
 
     renderer.setSize(window.innerWidth, window.innerHeight)
 
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    controls.enablePan = false
+    controls.maxDistance = 100
+    controls.minDistance = 40
+    controls.enableDamping = true
+
     document.body.appendChild(renderer.domElement)
+
+
+    const grid = new THREE.GridHelper(500, 20)
+    scene.add(grid)
+
 
     function render() {
         renderer.render(scene, camera)
+        controls.update()
     }
 
     function animate() {
