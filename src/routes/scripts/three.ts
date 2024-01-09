275bb4ef9174fb34b79e391f2448c2c52218e39a
defrost <118262248+defrxst@users.noreply.github.com>
2024-01-09 20:25:01 +0000

    Update three.ts

--- src/routes/scripts/three.ts
+++ src/routes/scripts/three.ts
@@ -6,30 +6,43 @@ if(browser) {
     const canvas = document.getElementById('canvas')
 
     const scene = new THREE.Scene()
-    const renderer = new THREE.WebGLRenderer()
+    const renderer = new THREE.WebGLRenderer({antialias: true})
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
     const controls = new OrbitControls(camera, renderer.domElement)
 
     controls.enableDamping = true
     controls.enableZoom = false
     controls.enablePan = false
+    controls.enableRotate = false
 
-    camera.position.setZ(30)
-    camera.position.setY(40)
+    camera.position.z = 40
+    camera.position.y = 30
 
     renderer.setSize(window.innerWidth, window.innerHeight)
 
     document.body.appendChild(renderer.domElement)
 
-    function moveCamera() {
-        camera.position.y -= 1
-      }      
-
-    document.body.addEventListener('scroll', moveCamera)
-      
-
-    const grid = new THREE.GridHelper(5000, 200)
+    
+    const grid = new THREE.GridHelper(999, 40)
     scene.add(grid)
+
+    function moveCamera() {
+        if(camera.position.y <= 5) {
+            camera.position.z += 3
+        } else {
+            camera.position.y -= 2
+        }
+    }      
+      
+    document.body.addEventListener('wheel', moveCamera, {passive: false})
+
+    document.body.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+
+        camera.updateProjectionMatrix();
+    
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    })
 
     function render() {
         renderer.render(scene, camera)
